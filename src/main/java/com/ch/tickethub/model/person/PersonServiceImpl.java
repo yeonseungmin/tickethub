a23b8d610268ce6afabd5b9af0c3f039023e9f2e
kaoru0705 <kaoru9875@gmail.com>
2025-12-31 20:00:51 +0900

    인물 등록 방식 FileUtil->FileManager로 변경 및 FileUtil.getRootDir(); 추가 커밋

--- src/main/java/com/ch/tickethub/model/person/PersonServiceImpl.java
+++ src/main/java/com/ch/tickethub/model/person/PersonServiceImpl.java
@@ -3,6 +3,7 @@ package com.ch.tickethub.model.person;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,24 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.ch.tickethub.dto.Person;
 import com.ch.tickethub.exception.PersonException;
+import com.ch.tickethub.util.FileManager;
 import com.ch.tickethub.util.FileUtil;
 
+import lombok.extern.slf4j.Slf4j;
+
 /* DB transaction RuntimeException 발생 rollback */
+@Slf4j
 @Transactional
 @Service
 public class PersonServiceImpl implements PersonService{
 
 	@Autowired
 	PersonDAO personDAO;
+	
+	@Autowired
+	FileManager fileManager;
+	
+	private String rootDir = FileUtil.getRootDir() + "/person";
 	
 	@Override
 	public void regist(List<String> nameList, List<MultipartFile> imgList) throws PersonException{
@@ -29,22 +39,32 @@ public class PersonServiceImpl implements PersonService{
 			MultipartFile img = imgList.get(i);
 			
 			// 실패할 경우 저장소에 있는 이미지 다시 삭제 해야 됨
-			List<String>savedFiles = new ArrayList();
+			List<Integer>savedFiles = new ArrayList();
+			
+			String personFilename = UUID.randomUUID() + "." + fileManager.getExtend(img.getOriginalFilename());
 			
 			try {
-				String url = FileUtil.saveFile("C:/tickethub/performance/person", img);
-				savedFiles.add(url);
 				
 				Person person = new Person();
 				
 				person.setPerson_name(name);
-				person.setProfile_url(url);
+				person.setProfile_url(personFilename);
 				
 				personDAO.insert(person);
+				log.debug("insert 직후 mybatis selectKey 동작 후 person의 person_id값은 " + person.getPerson_id());
+				
+				savedFiles.add(person.getPerson_id());
+				
+				String dirName = rootDir + "/p" + person.getPerson_id();
+				fileManager.makeDirectory(dirName);
+				
+				fileManager.save(img, dirName, personFilename);
 				
 			} catch (Exception e) {
-				for(String url: savedFiles) {
-					FileUtil.deleteFile(url);
+				for(int person_id: savedFiles) {
+					String dirName = rootDir + "/p" + person_id;
+					
+					fileManager.remove(dirName);
 				}
 				
 				throw new PersonException("인물 등록 실패", e);
@@ -53,7 +73,5 @@ public class PersonServiceImpl implements PersonService{
 		}
 		
 	}
-	
-
 
 }
