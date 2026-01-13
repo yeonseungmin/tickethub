f2afc42ffef1522626205b58084eb86d23dd47aa
nam-yeowon <nam091004@naver.com>
2026-01-13 10:34:29 +0900

    date 타입 해결

--- src/main/java/com/ch/tickethub/dto/Member.java
+++ src/main/java/com/ch/tickethub/dto/Member.java
@@ -2,6 +2,8 @@ package com.ch.tickethub.dto;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -11,6 +13,7 @@ public class Member {
 	private String passwordHash; //일반 로그인 패스워드
 	private String name;
 	private String email;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date birthDate;
 	private String phone;
 	private String zipCode;
